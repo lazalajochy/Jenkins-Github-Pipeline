5e1e96d129eb599bee8fd26a4cff79886a05a838
lazalajochy <jrosario@intellisysdcorp.com>
2025-03-01 12:21:24 -0400

    error

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.get('/', function (req, res) {
 
 var server = app.listen(3000, function () {
   var host = server.address().address;
-  var port = server.address().port;
+  var port = server.address().ports;
 
   console.log('App escuchando a http://%s:%s', host, port);
 });
